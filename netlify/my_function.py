cd0e27358c196c122761269e61f5409dac94b937
tanishq <bhambri26@users.noreply.github.com>
2025-10-22 03:58:58 +0530

    Create my_function.py

---
+++ netlify/my_function.py
@@ -0,0 +1,8 @@
+    # netlify/functions/my_function.py
+    import json
+
+    def handler(event, context):
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'Hello from Netlify Python Function!'})
+        }
